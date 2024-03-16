219eb43d6e67334e2b7b9dc698f77848f477c129
Prince <43872697+prince475@users.noreply.github.com>
2024-03-17 02:16:33 +0300

    Declaring variables to access our HTML elements that we need to control

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// Accessing our HTML elements by their Id's or classes
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
