5fae2790da4c4e2198fc038b3a8d3b9ff09d4e7d
Prince <43872697+prince475@users.noreply.github.com>
2024-03-20 22:56:30 +0300

    To get a count of the number of entries, you can query by text inputs.

--- script.js
+++ script.js
@@ -38,5 +38,5 @@ function isInvalidInput(str) {
 // Allowing users to add entry function
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll()
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 }
