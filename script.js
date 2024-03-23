90d67866bf34b4c6d28e5f9725010a324b9212ea
Prince <43872697+prince475@users.noreply.github.com>
2024-03-23 23:59:22 +0300

    Adding an event listener to the addEntryButton

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function isInvalidInput(str) {
 // Allowing users to add entry function
 function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
@@ -46,4 +46,5 @@ function addEntry() {
    <input type="number" min="0"  id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
   `;
   targetInputContainer.innerHTML += HTMLString;
+  addEntryButton.addEventListener("click", addEntry);
 }
