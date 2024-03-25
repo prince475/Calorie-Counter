53e924d6b2dd0601146a1647941f6d12a72a506e
Prince <43872697+prince475@users.noreply.github.com>
2024-03-25 22:30:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,11 @@ function addEntry() {
 function getCaloriesFromInputs(list) {
   let calories = 0;
   for (const item of list) {
-    
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+
+    }
   }
-  const currVal = item.value
 }
