6c1c3126a3265b4eabccd7c640edb911557a3f2a
Prince <43872697+prince475@users.noreply.github.com>
2024-03-29 15:08:35 +0300

    Querying for our number inputs in the different user input categories that we have and assigning a variable to them as well.

--- script.js
+++ script.js
@@ -53,7 +53,11 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
-  let breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]')
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]')
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');  
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
 }
 
 // Getting our inputs
